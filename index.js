70a2f5e01a6062255aa4c0820d81f0456330b49e
Iván Vega <vega_jib@hotmail.com>
2021-10-29 09:01:03 -0500

    funcional hasta listado de proveedores en el html

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from 'express';
+const app = express()
+import {agregarProveedor, obtenerProveedores} from './src/mysql.js'
+import path from 'path';
+let datos
+
+
+app.listen('8000',function(){
+    console.log('servidor en puerto 8000')
+})
+
+//configuracion ejs gestos de plantillas
+app.set('view engine', 'ejs');
+app.set('views', './vistas')
+
+
+
+//configuaracion archivos estaticos
+app.use(express.static('./vistas'))
+app.use(express.static('./src'))
+app.use(express.static('./css'))
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+    res.redirect('frm_proveedores')  
+});
+
+app.get('/frm_proveedores', function(req, res){
+    datos= obtenerProveedores()
+    console.log(datos[0].nombre)
+    res.render('frm_proveedores',{proveedores: datos})
+    
+});
+
+app.get('/agregar/:nombre/:vendedor/:direccion/:telefono/:informacion', function(req, res){
+    let nombre= req.params.nombre
+    let vendedor= req.params.vendedor
+    let direccion= req.params.direccion
+    let telefono= req.params.telefono
+    let informacion= req.params.informacion
+    agregarProveedor(nombre,vendedor,direccion,telefono,informacion)
+    res.redirect('/')
+});
